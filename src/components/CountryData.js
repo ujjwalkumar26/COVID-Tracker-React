84b5945825a1692a1c80e3134b93ec2a8b4dcddd
Ujjwal <ujjwal26092002@gmail.com>
2021-05-25 19:31:01 +0530

    countries flag add

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -1,22 +1,33 @@
 import '../styles/State.css'
 import React from 'react'
-//import {useEffect,useState} from 'react';
-//import axios from 'axios'; 
+import {useEffect,useState} from 'react';
+import axios from 'axios'; 
 function CountryData(params){
-    // var[countryInfo,setCountryInfo] = useState()
-    // var countryName=params.Country;
-    // const apiURL = `https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`;
-    //     const fetchData = async () => {
-    //     const response = await axios.get(apiURL)
-    //     var Countrydata = response.data[0]; 
-    //     await setCountryInfo(Countrydata);
-    //     console.log(CountryData);
-    //     }
-    //     useEffect(()=>{
-    //        fetchData();
-    //      },[]);
+    
+    var [img,setImg]=useState()
+    //var isImg=false;
+    var countryName=params.params.Country;
+    //console.log(countryName)
+     const apiURL = 'https://restcountries.eu/rest/v2/name/'+countryName+'?fullText=true';
+     //console.log(apiURL);
+        const fetchData = async () => {
+            try{
+                const response = await axios.get(apiURL)
+                var Countrydata = response.data[0]; 
+                //console.log(CountryData);// 
+                //eslint-disable-next-line
+                setImg(<img src={Countrydata.flag} className='flag'/>)
+            }
+            catch(err){
+                if(err)console.log(err);
+            }
+         }
+         useEffect(()=>{
+            fetchData();// eslint-disable-next-line
+          },[]);
     return(<div className="box effect5">
     <h1>{params.params.Country}</h1>
+        {img}
         <h5>Total confirmed cases: {params.params.TotalConfirmed}</h5>
         <h5>Total Deaths: {params.params.TotalDeaths}</h5>
         <h5>Total Recovered: {params.params.TotalRecovered}</h5>
